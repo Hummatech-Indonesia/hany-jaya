ca08d6e8777f6261a5797bf62cd695cd3cee89e3
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-05-13 22:56:50 +0700

    push

--- database/migrations/2014_10_12_000002_create_users_table.php
+++ database/migrations/2014_10_12_000002_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignUuid('store_id')->constrained()->restrictOnDelete()->cascadeOnUpdate()->nullable();
+            $table->foreignUuid('outlet_id')->constrained()->restrictOnDelete()->cascadeOnUpdate()->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
