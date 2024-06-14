3855cabda39c27fb013871ffa93cd44fa04d522e
frendikawa <frsnaifla@gmail.com>
2024-06-14 08:40:08 +0700

    riwayat penjualan dashboard admin

--- resources/views/dashboard/selling/history.blade.php
+++ resources/views/dashboard/selling/history.blade.php
@@ -1,9 +1,25 @@
 @php
     use App\Helpers\FormatedHelper;
 @endphp
-@extends('dashboard.layouts.cashier')
+@extends('dashboard.layouts.dashboard')
 @section('content')
     <div class="container-fluid">
+        <div class="card bg-light-info shadow-none position-relative overflow-hidden">
+            <div class="card-body px-4 py-3">
+                <div class="row align-items-center">
+                    <div class="col-9">
+                        <h4 class="fw-semibold mb-8">Riwayat Penjualan</h4>
+                        <p>Riwayat Penjualan di toko anda.</p>
+                    </div>
+                    <div class="col-3">
+                        <div class="text-center mb-n5">
+                            <img src="{{ asset('assets/images/breadcrumb/ChatBc.png') }}" alt=""
+                                class="img-fluid mb-n4" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="widget-content searchable-container list mt-4">
             <div class="card card-body">
                 <div class="table-responsive">
@@ -89,7 +105,7 @@
             let pay = $(this).data('pay');
             console.log(detailSellings);
             if (status_payment == 'debt') {
-                $('.sembuntikan').hide();
+                $('.sembuyikan').hide();
             } else {
                 $('#return').html(formatRupiah(returns));
                 $('#pay').html(formatRupiah(pay));
