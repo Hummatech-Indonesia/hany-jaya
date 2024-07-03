a38e98938addd955cc41c4550a78bfc8e9f6bb9e
frizehero <bag4sk4r4@gmail.com>
2024-07-03 13:35:40 +0700

    mod: update button

--- resources/views/dashboard/category/index.blade.php
+++ resources/views/dashboard/category/index.blade.php
@@ -93,17 +93,15 @@
                                         <td>
                                             <div class="d-flex align-items-center gap-2">
                                                 <div class="d-flex align-items-center gap-2">
-                                                    <a class="dropdown-item btn-update" href="#"
-                                                        data-category="{{ $category }}"
-                                                        data-url="{{ route('admin.categories.update', $category->id) }}">
-                                                        <i class="fs-4 ti ti-edit text-warning"></i>Edit
-                                                    </a>
+                                                    <button type="button" class="btn btn-sm btn-primary btn-update" data-category="{{ $category }}"
+                                                    data-url="{{ route('admin.categories.update', $category->id) }}">
+                                                        <i class="fs-4 ti ti-edit"></i> <span class="d-none d-md-inline">Edit</span>
+                                                    </button>
                                                 </div>
                                                 <div class="d-flex align-items-center gap-2">
-                                                    <a class="dropdown-item btn-delete" href="#"
-                                                        data-url="{{ route('admin.categories.destroy', $category->id) }}">
-                                                        <i class="fs-4 ti ti-trash text-danger"></i>Delete
-                                                    </a>
+                                                    <button type="button" class="btn btn-sm btn-danger btn-delete" data-url="{{ route('admin.categories.destroy', $category->id) }}">
+                                                        <i class="fs-4 ti ti-trash"></i> <span class="d-none d-md-inline">Hapus</span>
+                                                    </button>
                                                 </div>
                                             </div>
                                         </td>
