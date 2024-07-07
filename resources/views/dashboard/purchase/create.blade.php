d4075e6a77ac7befc917c68c7ccf49c16946596a
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-07-07 23:55:28 +0700

    mod: change product list to table

--- resources/views/dashboard/purchase/create.blade.php
+++ resources/views/dashboard/purchase/create.blade.php
@@ -76,60 +76,30 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div id="education_fields" class="my-1"></div>
-                                <div class="col-sm-2">
-                                    <div class="mb-3">
-                                        <label class="mb-2" for="product_id" style="font-size: 0.8rem">Pilih
-                                            Produk <small class="text-danger">*</small></label>
-                                        <select name="product_id[]" class="select_product form-control" id="">
-                                            <option value="">Pilih Produk</option>
-                                        </select>
-                                    </div>
+
+                            <div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="text-bold">Produk</div>
+                                    <button type="button" class="btn btn-success" id="btn-add-product">+ Tambah</button>
                                 </div>
-                                <div class="col-sm-2">
-                                    <div class="mb-3">
-                                        <label class="mb-2" for="product_unit_id" style="font-size: 0.8rem">Pilih
-                                            Satuan <small class="text-danger">*</small></label>
-                                        <select name="product_unit_id[]" class="form-control" id="product_unit_id">
-                                            <option value="Pilih Satuan">
-                                                Pilih Satuan
-                                            </option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="mb-3">
-                                        <label class="d-flex gap-2 align-items-center mb-2" style="font-size: 0.8rem">Harga
-                                            Beli per Satuan <small class="text-danger">*</small></label>
-                                        <input type="number" class="form-control price-per-unit" id="Age"
-                                            name="buy_price_per_unit[]" placeholder="10" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="mb-3">
-                                        <label class="d-flex gap-2 align-items-center mb-2" style="font-size: 0.8rem">Jumlah
-                                            Pembelian <small class="text-danger">*</small></label>
-                                        <input type="number" class="form-control quantity" id="Age" name="quantity[]"
-                                            placeholder="10" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <label class="mb-2" for="" style="font-size: 0.8rem">Total Harga
-                                        Pembelian <small class="text-danger">*</small></label>
-                                    <div class="mb-3">
-                                        <input readonly name="buy_price[]" type="number" class="form-control"
-                                            value="0" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-2">
-                                    <div class="" style="margin-top: 1.35rem">
-                                        <button id="add_click"
-                                            class="btn btn-success font-weight-medium waves-effect waves-light mt-2"
-                                            type="button">
-                                            <i class="ti ti-circle-plus fs-5"></i>
-                                        </button>
-                                    </div>
+                                <div class="table-responsive">
+                                    <table class="table align-middle">
+                                        <thead>
+                                            <tr>
+                                                <th>Produk <span class="text-danger">*</span></th>
+                                                <th>Satuan <span class="text-danger">*</span></th>
+                                                <th>Harga per satuan <span class="text-danger">*</span></th>
+                                                <th>Jumlah <span class="text-danger">*</span></th>
+                                                <th>Total Harga <span class="text-danger">*</span></th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody  id="tb-products">
+                                            <tr>
+                                                <th colspan="5" class="text-center text-muted text-bold">-- belum ada produk dipilih --</th>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                             <button class="btn btn-info rounded-md px-4 mt-3" type="submit">
@@ -141,60 +111,86 @@
             </div>
         </div>
     </div>
-    @endsection @section('script')
-    @include('dashboard.purchase.widgets.repeater')
+@endsection
+@section('script')
+    <script src="{{asset('assets/js/number-format.js')}}"></script>
     <script>
         $(document).ready(function() {
-            $(".select_product").change(function() {
-                var productId = $(this).val();
+            let product_list = []
+            $(document).on('change', '#supplier_id',function() {
+                $.ajax({
+                    url: `{{ route('admin.supplier.product.index', ['supplier' => '']) }}/${$('#supplier_id').val()}`,
+                    type: "GET",
+                    success: function(response) {
+                        product_list = response.data
+                    },
+                    error: function(xhr) {
+                        product_list = [];
+                    },
+                });
+            })
+            
+            $(document).on("click", "#btn-add-product", function () {
+                if(!$('#supplier_id').val()) return;
+                let last_index = $('#tb-products tr[data-index]').last().data('index')
+
+                let current_index = last_index ? last_index+1 : 1
+                
+                let str_products = '<option value="" selected disabled>-- pilih produk --</option>'
+                product_list.forEach((data) => {
+                    str_products += `<option value="${data.product_id}">${data.product}</option>`
+                })
+
+                let new_tr = `
+                    <tr data-index="${current_index}">
+                        <td>
+                            <select name="product_id[]" class="select_product form-select" required>${str_products}</select>
+                            <input type="hidden" name="buy_price_per_unit[]" required />
+                            <input type="hidden" name="quantity[]" required />
+                            <input type="hidden" name="buy_price[]" required />
+                        </td>
+                        <td>
+                            <select name="product_unit_id[]" class="form-select" id="product_unit" required>
+                                <option value="" selected disabled>-- pilih satuan --</option>
+                            </select>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control format-number price-per-unit" placeholder="Harga beli persatuan" required/>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control format-number quantity" placeholder="Jumlah dibeli" required/>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control format-number buy-price" placeholder="Jumlah dibeli" readonly required/>
+                        </td>
+                        <td><button type="button" class="btn btn-danger btn-delete">-</button></td>
+                    </tr>
+                `
+
+                if(last_index) {
+                    $('#tb-products').append(new_tr)
+                } else {
+                    $('#tb-products').html(new_tr)
+                }
+            });
+
+            $(document).on("click", ".btn-delete", function() {
+                $(this).parent().parent().remove()
+            })
+
+            $(document).on("change", ".select_product", function() {
+                let productId = $(this).val();
+                let options = `<option value="" disabled selected>-- pilih satuan --</option>`
+                let unit_input = $(this).parent().parent().find('#product_unit')
+                
                 $.ajax({
                     url: `{{ route('admin.product.unit.index', ['product' => '']) }}/${productId}`,
                     type: "GET",
                     success: function(response) {
-                        console.log(response);
-                        $("#product_unit_id").empty();
-                        $("#product_unit_id").append(
-                            '<option value="">Pilih Satuan</option>'
-                        );
                         response.data.forEach(function(item) {
-                            $("#product_unit_id").append(
-                                `<option value="${item.id}">${item.unit}</option>`
-                            );
+                            options += `<option value="${item.id}">${item.unit}</option>`
                         });
-                    },
-                    error: function(xhr) {
-                        console.log(xhr.responseText);
-                    },
-                });
-            });
-        });
-
-        $(document).ready(function() {
-            $("#supplier_id").change(function() {
-                // clear form cart
-                $(".form-repeater").remove();
-                $("#product_unit_id").empty();
-                $("#product_unit_id").append(
-                    '<option value="">Pilih Satuan</option>'
-                );
-                $("input[name='buy_price_per_unit[]']").val("");
-                $("input[name='quantity[]']").val("");
-                $("input[name='buy_price[]']").val("");
-
-                var supplierId = $(this).val();
-                $.ajax({
-                    url: `{{ route('admin.supplier.product.index', ['supplier' => '']) }}/${supplierId}`,
-                    type: "GET",
-                    success: function(response) {
-                        $(".select_product").empty();
-                        $(".select_product").append(
-                            '<option value="">Pilih Produk</option>'
-                        );
-                        response.data.forEach(function(item) {
-                            $(".select_product").append(
-                                `<option value="${item.product_id}">${item.product}</option>`
-                            );
-                        });
+                        unit_input.html(options)
                     },
                     error: function(xhr) {
                         console.log(xhr.responseText);
@@ -202,39 +198,29 @@
                 });
             });
 
-            $(".price-per-unit").on("input", function() {
-                var pricePerUnit = $(this).val();
-                var quantity = $(this)
-                    .parent()
-                    .parent()
-                    .parent()
-                    .find(".quantity")
-                    .val();
-                var totalPrice = pricePerUnit * quantity;
-                $(this)
-                    .parent()
-                    .parent()
-                    .parent()
-                    .find('input[name="buy_price[]"]')
-                    .val(totalPrice);
-            });
+            $(document).on("input", ".format-number", function() {
+                if(unformatNum($(this).val()) < 0) $(this).val(0)
+                else $(this).val(formatNum($(this).val()))
+            })
 
-            $(".quantity").on("input", function() {
-                var quantity = $(this).val();
-                var pricePerUnit = $(this)
-                    .parent()
-                    .parent()
-                    .parent()
-                    .find(".price-per-unit")
-                    .val();
-                var totalPrice = pricePerUnit * quantity;
-                $(this)
-                    .parent()
-                    .parent()
-                    .parent()
-                    .find('input[name="buy_price[]"]')
-                    .val(totalPrice);
-            });
-        });
+            $(document).on("input", ".price-per-unit", function() {
+                let unformat_input = $(this).parent().parent().find('[name=buy_price_per_unit\\[\\]]')
+                unformat_input.val(unformatNum($(this).val()))
+                changeTotal($(this))
+            })
+            $(document).on("input", ".quantity", function() {
+                let unformat_input = $(this).parent().parent().find('[name=quantity\\[\\]]')
+                unformat_input.val(unformatNum($(this).val()))
+                changeTotal($(this))
+            })
+            function changeTotal(el) {
+                let tr = el.parent().parent()
+                let price = unformatNum(tr.find(".price-per-unit").val())
+                let qty = unformatNum(tr.find(".quantity").val())
+                let total = price * qty
+                tr.find('[name=buy_price\\[\\]]').val(total)
+                tr.find('.buy-price').val(formatNum(total))
+            }
+        })
     </script>
 @endsection
