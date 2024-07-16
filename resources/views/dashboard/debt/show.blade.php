3a54b8de6b8b8aa068b765b935d65c24c0bc521b
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-07-16 14:25:31 +0700

    mod: add datatable in detail debt

--- resources/views/dashboard/debt/show.blade.php
+++ resources/views/dashboard/debt/show.blade.php
@@ -20,8 +20,8 @@
             <div class="card-body px-4 py-3">
                 <div class="row align-items-center">
                     <div class="col-9">
-                        <h4 class="fw-semibold mb-8">Detail Hutang Piutang</h4>
-                        <p>Hutang piutang oleh pelanggan.</p>
+                        <h4 class="fw-semibold mb-8">Hutang Piutang</h4>
+                        <p>Hutang piutang oleh <span class="fw-bolder">{{ $buyer->name }} - {{ $buyer->address }}</span>.</p>
                     </div>
                     <div class="col-3">
                         <div class="text-center mb-n5">
@@ -35,29 +35,7 @@
 
         <div class="card">
             <div class="table-responsive card-body">
-                <table class="table align-middle">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Tanggal</th>
-                            <th>Tipe</th>
-                            <th>Nominal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>1</th>
-                            <td>12 Juli 2024</td>
-                            <td><span class="badge fs-2 bg-primary">pembayaran</span></td>
-                            <td>Rp 200.000</td>
-                        </tr>
-                        <tr>
-                            <th>2</th>
-                            <td>10 Juli 2024</td>
-                            <td><span class="badge fs-2 bg-warning">piutang</span></td>
-                            <td>Rp 200.000</td>
-                        </tr>
-                    </tbody>
+                <table class="table align-middle" id="tb-full-debt-history">
                 </table>
             </div>
         </div>
@@ -77,4 +55,43 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-colvis-3.0.2/b-html5-3.0.2/datatables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            const tb_unit = $('#tb-full-debt-history').DataTable({
+                processing: true,
+                serverSide: true,
+                order: [[1, 'desc']],
+                ajax: {
+                    url: "{{route('data-table.list-detail-debt', $buyer->id)}}"
+                },
+                columns: [
+                    {
+                        data: "DT_RowIndex",
+                        title: "No",
+                        searchable: false,
+                        orderable: false
+                    }, {
+                        data: "date",
+                        title: "Tanggal",
+                        render: (data, type) => {
+                            return moment(data).format('DD MMMM YYYY')
+                        }
+                    }, {
+                        data: "type",
+                        title: "Jenis",
+                        render: (data, type) => {
+                            if(data == 'debt') return '<span class="badge bg-warning fs-2">Hutang</span>'
+                            return '<span class="badge bg-primary fs-2">Pembayaran</span>'
+                        }
+                    }, {
+                        data: "amount",
+                        title: "Total",
+                        render: (data, type) => {
+                            return "Rp "+formatNum(data)
+                        }
+                    }
+                ]
+            })
+        })
+    </script>
 @endsection
