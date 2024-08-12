a9c98ee84835b3b2987770ddd3ba1e2a2c58eda1
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-08-12 14:03:22 +0700

    mod: dd

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -19,7 +19,7 @@ class Unit extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'unit_id', 'id');
+        return $this->belongsTo(Product::class);   
     }
 
     public function productUnit()
