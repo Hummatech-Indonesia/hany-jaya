b646906d86b75c01782af1e22e46ed6845dbe110
frizehero <bag4sk4r4@gmail.com>
2024-08-12 13:39:29 +0700

    fix: add validation for quantity and price in add product

--- app/Http/Requests/Admin/ProductRequest.php
+++ app/Http/Requests/Admin/ProductRequest.php
@@ -20,13 +20,13 @@ class ProductRequest extends FormRequest
             'unit_id' => 'required|array',
             'unit_id.*' => 'required|exists:units,id',
             'quantity_in_small_unit' => 'required|array',
-            'quantity_in_small_unit.*' => 'required',
+            'quantity_in_small_unit.*' => 'required|numeric|min:1',
             'small_unit_id' => 'required|exists:units,id',
             'code' => ['required', Rule::unique('products', 'code')->ignore($this->product)],
             'name' => 'required|max:255',
             'quantity' => 'nullable',
             'selling_price' => 'required|array',
-            'selling_price.*' => 'required',
+            'selling_price.*' => 'required|numeric|min:1',
             'image' => 'nullable|mimes:png,jpg,jpeg',
         ];
     }
@@ -46,6 +46,8 @@ class ProductRequest extends FormRequest
             'unit_id.*.exists' => 'Satuan tidak ditemukan',
             'quantity_in_small_unit.required' => 'Jumlah satuan kecil wajib diisi',
             'quantity_in_small_unit.*.required' => 'Jumlah satuan kecil wajib diisi',
+            'quantity_in_small_unit.*.numeric' => 'Jumlah satuan kecil harus angka',
+            'quantity_in_small_unit.*.min' => 'Jumlah satuan kecil minimal 1',
             'small_unit_id.required' => 'Satuan kecil wajib dipilih',
             'small_unit_id.exists' => 'Satuan kecil tidak ditemukan',
             'code.required' => 'Kode wajib diisi',
@@ -54,7 +56,28 @@ class ProductRequest extends FormRequest
             'name.max' => 'Nama maksimal 255 karakter',
             'selling_price.required' => 'Harga jual wajib diisi',
             'selling_price.*.required' => 'Harga jual wajib diisi',
+            'selling_price.*.numeric' => 'Harga harus angka',
+            'selling_price.*.min' => 'Harga minimal 1',
             'image.mimes' => 'Format gambar harus png, jpg, jpeg',
         ];
+    }
+
+    public function prepareForValidation()
+    {
+        if ($this->selling_price) {
+            $intArray = [];
+            foreach ($this->selling_price as $value) $intArray[] = intval($value);
+            $this->merge([
+                'selling_price' => $intArray
+            ]);
+        }
+
+        if ($this->quantity_in_small_unit) {
+            $intArray = [];
+            foreach ($this->quantity_in_small_unit as $value) $intArray[] = intval($value);
+            $this->merge([
+                'quantity_in_small_unit' => $intArray
+            ]);
+        }
     }
 }
