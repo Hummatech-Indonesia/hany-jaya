1bbfcf1170e4ebaab66cecc428cf843973fffc73
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-08-29 11:49:14 +0700

    mod: responsive pie

--- resources/views/dashboard/home/widgets/pie-product.blade.php
+++ resources/views/dashboard/home/widgets/pie-product.blade.php
@@ -13,6 +13,17 @@
         },
         colors: ["#aaaaaa"],
         labels: ["kosong"],
+        responsive: [{
+            breakpoint: 990,
+            options: {
+                chart: {
+                    width: 300
+                },
+                legend: {
+                    position: 'right'
+                }
+            }
+        }]
     };
     
     var pie_chart = new ApexCharts(document.querySelector("#pieChart"), options);
